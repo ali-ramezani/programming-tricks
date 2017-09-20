c7a58d0d6de9e4242fd166a5f0cb4e444f1b45b0
ramezaniua <ramezaniua@gmail.com>
2017-09-20 13:43:04 +0200

    [v2] Python like function call for C

--- codes/python-like-argument.c
+++ codes/python-like-argument.c
@@ -1,12 +1,12 @@
  
 #include <stdio.h>
 
-double f(int i){return i+1.5;}
-#define f(...) ({double ret;int i; __VA_ARGS__; ret=f(i);ret;})
+double f(int i,int j){printf("\ni=%d j=%d => i+j/2=%lg",i,j,i+j/2.0); return i+j/2.0;}
+#define f(...) ({double ret;int i,j=5; __VA_ARGS__; ret=f(i,j);ret;})
 
 int main (int argc, char **argv)
 {
-double g=f(i=5);
-printf("%lf\n",f(i=4));
+double g=f(i=6);
+printf("\n\t%lg\n\t%lg\n",f(j=3,i=4),g);
   return 1;
 }
